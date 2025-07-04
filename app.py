9c0b842b8ad7339186bb9c45f2c10030799396b7
Vikram <vvvi31110@gmail.com>
2025-07-04 21:09:15 +0530

    toCsv

--- app.py
+++ app.py
@@ -3,21 +3,16 @@ import pandas as pd
 import os
 
 app = Flask(__name__)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# Construct the full path to your Excel file
-# This assumes semres.xlsx is in the same folder as app.py
-excel_file_path = os.path.join(BASE_DIR, 'semres.xlsx')
-
-df = pd.read_excel(excel_file_path)
+    
+df = pd.read_csv('semres.csv')
+df.set_index('USN', inplace=True)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         usn = request.form.get("usn").strip().upper()
-        row = df[df['USN'] == usn]
-        if not row.empty:
-            row = row.iloc[0].to_dict()
+        if usn in df.index:
+            row = df.loc[usn].to_dict()
             return render_template("result.html", usn=usn, details=row)
         else:
             error = f"USN '{usn}' not found."
@@ -25,4 +20,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
